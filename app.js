1e7ff5bd45bf7a256666235db62c52064177fd00
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-12 12:48:14 +0530

    changes commited

--- app.js
+++ app.js
@@ -17,6 +17,40 @@ app.post("/add", (req, res) => {
     res.json({ "status": "success" })
 })
 
+app.post("/search", (req, res) => {
+
+    let input = req.body
+    coursemodel.find(input).then(
+        (data) => {
+            res.json(data)
+        }
+
+    ).catch(
+        (error) => {
+            res.json(error)
+        }
+    )
+})
+
+
+app.post("/delete", (req, res) => {
+    let input = req.body
+    coursemodel.findByIdAndDelete(input._id).then(
+        (response) => {
+            res.json({ "status": "success" })
+
+
+
+        }
+
+    ).catch(
+        (erro) => {
+            res.json({ "status": "error" })
+        }
+
+    )
+})
+
 app.get("/view", (req, res) => {
 
     coursemodel.find().then(
@@ -31,7 +65,9 @@ app.get("/view", (req, res) => {
     )
 })
 
-app.listen(8067, () => {
+
+
+app.listen(8083, () => {
     console.log("server started")
 
 })
